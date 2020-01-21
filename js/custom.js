3629bb03e7dcf72dad749cc4abf558f909a8259f
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2020-01-21 08:21:03 -0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -28,30 +28,6 @@ $(function () {
             $grid.isotope({ filter: filterValue });
             $(this).addClass("active_filter").siblings().removeClass("active_filter");
         });
-        
-        /*--------------------------------
-            02. Preloader
-        ----------------------------------*/
-        $(".preloader").css('display','none');
-
-        setTimeout(function(){
-
-            $('html, body').animate({
-                scrollTop: 0
-            }, 1000, function () {
-                
-                $('.navbar-nav li:first-of-type').addClass('active').siblings().removeClass('active');
-                $('.page-loader').css('animation-name','');
-                $('.home .content').removeClass('opacity-zero');
-                $('.main_img').removeClass('img-shadow');
-                $('main').removeClass('load-main');
-                if (!Modernizr.cssmask){
-                    $(".page-loader,.preloader").fadeOut();
-                }
-
-            });
-
-        },1000);
 
         
     });//--- window(load) ---//
